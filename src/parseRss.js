718c9bbed2627f0001a4a63fb930c52bc2024c8c
iashchuk <221box@list.ru>
2020-09-10 00:47:22 +0300

    adds xml parser

---
+++ src/parseRss.js
@@ -0,0 +1,20 @@
+const parseInfo = (item) => {
+  const title = item.querySelector('title').textContent;
+  const link = item.querySelector('link').textContent;
+  const description = item.querySelector('description').textContent;
+  return { title, link, description };
+};
+
+
+export const parseRss = (xml) => {
+  const parser = new DOMParser();
+  const document = parser.parseFromString(xml, 'text/xml');
+  const items = [...document.querySelectorAll('item')];
+
+  return {
+    ...parseInfo(document), items: items.map(parseInfo),
+  };
+};
+
+
+export default parseRss;
